f2a808f49e0878c95019eb5d3476587e129ee9d8
Matteo-go <goramatteo@gmail.com>
2023-04-04 23:02:24 +0200

    [MGA][feat]: add fetch api to js file and redirect theme

--- app.js
+++
@@ -1,7 +0,0 @@
-import {getThemes} from "./api.js";
-
-import {displayThemes} from "./index-displayThemes.js";
-
-getThemes()
-    .then(results => displayThemes(results))
-    .catch( error => console.log(error))
